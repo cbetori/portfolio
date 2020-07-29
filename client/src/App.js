a42378d12ed67013345f840ff43af810bb757460
cbetori <cbetori@gmail.com>
2020-07-29 17:20:12 -0500

    Mobile CSS fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,11 @@ import React from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
-// import CoverPage from './components/CoverPage'
 
 const App = () => {
-	let vh = window.innerHeight * 0.01
-	document.documentElement.style.setProperty('--vh', vh + 'px')
-	// We listen to the resize event
-	window.addEventListener('resize', () => {
-		// We execute the same script as before
-		let vh = window.innerHeight * 0.01
-		document.documentElement.style.setProperty('--vh', `${vh}px`)
-	})
-	console.log(vh)
 	return (
 		<React.Fragment>
 			<Header />
-			{/* <CoverPage /> */}
 			<Body />
 			<Footer />
 		</React.Fragment>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,33 +2,17 @@ import React from 'react'
 import Button from './Button'
 
 const Header = () => {
-	let handleClick = id => {
+	let handleClick = (id) => {
 		let element = document.getElementById(id).offsetTop
 		window.scrollTo({ top: element - 100, behavior: 'smooth' })
 	}
 
-	let handleResume = () => {
-		fetch('/api/resume', {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-			.then(res => {
-				return res.json()
-			})
-			.then(result => window.open(result.resume))
-	}
-
 	return (
-		<div>
-			<div className='header'>
-				<Button onClick={() => handleClick('about')} value='About' />
-				<Button onClick={() => handleClick('projects')} value='Projects' />
-				<Button onClick={() => handleClick('skills')} value='Skill Set' />
-				<Button onClick={() => handleClick('education')} value='Education' />
-				{/* <Button onClick={() => handleResume()} value='Resume'></Button> */}
-			</div>
+		<div className='header'>
+			<Button onClick={() => handleClick('about')} value='About' />
+			<Button onClick={() => handleClick('projects')} value='Projects' />
+			<Button onClick={() => handleClick('skills')} value='Skill Set' />
+			<Button onClick={() => handleClick('education')} value='Education' />
 		</div>
 	)
 }
